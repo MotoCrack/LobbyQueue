155c8635cfb048b1661051bc92cd165db15e1b60
DevNatan <itroken99@gmail.com>
2018-07-08 22:39:47 -0300

    Exception

--- src/main/java/me/devnatan/lobbyqueue/libs/SocketUtil.java
+++ src/main/java/me/devnatan/lobbyqueue/libs/SocketUtil.java
@@ -8,29 +8,23 @@ import java.net.Socket;
 
 public class SocketUtil {
 
-    public static int getMaxPlayers(InetSocketAddress address) {
+    public static int getMaxPlayers(InetSocketAddress address) throws IOException {
         Socket socket = new Socket();
-        try {
-            socket.connect(address);
+        socket.connect(address);
 
-            DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-            DataInputStream dis = new DataInputStream(socket.getInputStream());
-            dos.write(0xFE);
+        DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
+        DataInputStream dis = new DataInputStream(socket.getInputStream());
+        dos.write(0xFE);
 
-            StringBuilder sb = new StringBuilder();
-            int i;
-            while((i = dis.read()) != -1) {
-                if(i != 0 && i > 16 && i != 255 && i != 23 && i != 24) {
-                    sb.append((char) i);
-                }
+        StringBuilder sb = new StringBuilder();
+        int i;
+        while((i = dis.read()) != -1) {
+            if(i != 0 && i > 16 && i != 255 && i != 23 && i != 24) {
+                sb.append((char) i);
             }
-
-            return Integer.valueOf(sb.toString().split("§")[2]);
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
-        return 0;
+        return Integer.valueOf(sb.toString().split("§")[2]);
     }
 
 }
